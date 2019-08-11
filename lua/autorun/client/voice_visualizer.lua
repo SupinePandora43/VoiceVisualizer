cf95bf86e3f8646b246a0ac5b43c26543c18d9e4
Deyan Dobromirov <dvdvideo123@gmail.com>
2019-08-11 12:46:39 +0300

    Fixed: Localized `vv` to prevent global value injection
    Updated: All blank operators are now spaces instead of tabs/spaces
    Updated: visualizer with proper coloring

--- lua/autorun/client/voice_visualizer.lua
+++ lua/autorun/client/voice_visualizer.lua
@@ -1,55 +1,54 @@
-// Code based on Garry's Base Gamemode
-vv = {}
-local PANEL = {}
+-- Code based on Garry's Base Gamemode
+local vv                = {}
+local PANEL             = {}
 local PlayerVoicePanels = {}
-// Color
+
+-- Color
 vv.BarColor = {
-    [0] = Color(255, 0, 0), -- Over 0% -> Red
-    [25] = Color(255, 255, 0), -- Over 25 % -> Yellow
-    [50] = Color(0, 255, 0) -- Over 50% -> Green
+  [0]  = Color(0  , 255, 0)  -- Low amplitude 0% -> Green
+  [25] = Color(255, 255, 0), -- Middle amplitude 25 % -> Yellow
+  [50] = Color(255,   0, 0), -- High amplitude 50% -> Red
 }
 
-
-
-// Adjust Bar Height
+-- Adjust Bar Height
 -- This is a PERFECT setting - Be sure before changing it
 -- Default: 40
 vv.BarHeightMultiplier = CreateClientConVar("vv_bar_height", "40", true, false, "Adjust Bar Height")
 
-// Update Rate
+-- Update Rate
 -- If you want it faster, increase the rate
 -- If you want it slower, decrease the rate
 -- Default: 0.1
 vv.UpdateRate = CreateClientConVar("vv_update_rate", "0.1", true, false, "Update Rate\nIf you want it faster, increase the rate\nIf you want it slower, decrease the rate")
 
-// Single Bar Width
+-- Single Bar Width
 -- If you want more bars, decrease the value
 -- and increase the Bar Count
 -- Default: 5
 vv.SingleBarWidth = CreateClientConVar("vv_bar_width", "5", true, false, "Single Bar Width")
 
-// Bar Count
+-- Bar Count
 -- How many bars do you want to be displayed?
 -- Default: 30 (Perfect setting with bar width 5)
 vv.BarCount = CreateClientConVar("vv_bar_count", "30", true, false, "Bar Count")
 
-// Bar Distance
+-- Bar Distance
 -- Distance between 2 Bars
 -- Default: 2
 vv.BarDistance = CreateClientConVar("vv_bar_distance", "2", true, false, "Distance between Bars")
 
-// Background Color
+-- Background Color
 -- Background Color of the bar itself
 -- This HAS to be a function
 -- Default: Black
 vv.BackgroundColor = function(panel, ply)
     -- Tip if you have a TTT server
-    -- This will normalize the background color of the panel (Green for Inno, Blue for Detective and Red in private Traitor Voice Channel)
+    -- This will normalize the background color of the panel (Green for Info, Blue for Detective and Red in private Traitor Voice Channel)
     -- Change the line under me to: return panel.Color
     return Color(0,0,0)
 end
 
-// Name Color
+-- Name Color
 -- Color of the name
 -- This HAS to be a function
 -- Default: White
@@ -57,7 +56,7 @@ vv.NameColor = function(panel, ply)
     return Color(255,255,255)
 end
 
-// Name Font
+-- Name Font
 -- Font of the name
 -- This HAS to be a function
 -- Default: GModNotify
@@ -65,45 +64,46 @@ vv.NameFont = function(panel, ply)
     return "GModNotify"
 end
 
-// Call Gamemode Paint function
+-- Call Gamemode Paint function
 -- I highly recommend this stays turned off
 -- for example: If your gamemode draws a box, it will draw over the bar and stuff
 -- That would not be good
 -- Default: false (you should keep it that way)
 vv.CallGamemodePaintFunc = CreateClientConVar("vv_call_gm_paint_func", "0", true, false, "Call Gamemode Paint function\nI highly recommend this stays turned off\nfor example: If your gamemode draws a box, it will draw over the bar and stuff. That would not be good")
 
-// Gamemode Paint Function call
+-- Gamemode Paint Function call
 -- You have to test this function out on your gamemode
--- This sets wether the gamemode paint function should be called before (true) or after (false) my paint function
+-- This sets ether the gamemode paint function should be called before (true) or after (false) my paint function
 -- Again, test it out yourself
 -- NOTE: If you have set vv.CallGamemodePaintFunc to false, this will be ignored!
 -- Default: false
 vv.CallGamemodePaintFuncFirst = CreateClientConVar("vv_call_gm_paint_func_first", "0", true, false, "Gamemode Paint Function call\nThis sets wether the gamemode paint function should be called before (true) or after (false) my paint function\nNOTE: If you have set 'vv_call_gm_paint_func' to '0', this will be ignored!")
-/* 
+
+--[[
  * DO NOT EDIT ANYTHING FROM HERE !
- */
+]]
 
 function PANEL:Init()
-	self.Avatar = vgui.Create("AvatarImage", self)
-	self.Avatar:Dock(LEFT)
-	self.Avatar:SetSize(32, 32)
+  self.Avatar = vgui.Create("AvatarImage", self)
+  self.Avatar:Dock(LEFT)
+  self.Avatar:SetSize(32, 32)
 
-	self.Color = Color(0,0,0)
+  self.Color = Color(0,0,0)
 
-	self:SetSize(250, 32 + 8)
-	self:DockPadding(4, 4, 4, 4)
-	self:DockMargin(2, 2, 2, 2)
-	self:Dock(BOTTOM)
-    
-    self.Past = {}
+  self:SetSize(250, 32 + 8)
+  self:DockPadding(4, 4, 4, 4)
+  self:DockMargin(2, 2, 2, 2)
+  self:Dock(BOTTOM)
+
+  self.Past = {}
 end
 
-function PANEL:Setup(ply)  
-	self.ply = ply
-	--self.LabelName:SetText(ply:Nick())
-	self.Avatar:SetPlayer(ply)
-	
-	self.Color = team.GetColor(ply:Team())
+function PANEL:Setup(ply)
+  self.ply = ply
+  --self.LabelName:SetText(ply:Nick())
+  self.Avatar:SetPlayer(ply)
+
+  self.Color = team.GetColor(ply:Team())
     timer.Create("PanelThink" .. ply:UniqueID(), vv.UpdateRate:GetFloat(), 0, function()
         if self:Valid() then
             if self.UpdatePast ~= nil then
@@ -111,15 +111,15 @@ function PANEL:Setup(ply)
             end
         end
     end)
-	
-	self:InvalidateLayout()
-    
-    -- wow.. This is for the shitty gamemodes that overwrite my paint function -.-       
+
+  self:InvalidateLayout()
+
+    -- wow.. This is for the shitty gamemodes that overwrite my paint function -.-
     timer.Simple(0, function()
         if self ~= nil then
             if self:Valid() then
                 local PaintFunc = self.Paint
-                
+
                 self.Paint = function(s, w, h)
                     if s ~= nil then
                         if s:Valid() then
@@ -127,9 +127,9 @@ function PANEL:Setup(ply)
                             if PaintFunc ~= nil and vv.CallGamemodePaintFunc:GetBool() and vv.CallGamemodePaintFuncFirst:GetBool() == true then
                                 PaintFunc(s,w,h)
                             end
-                            
+
                             s:VVPaint(w, h)
-                            
+
                             -- Idiots
                             if PaintFunc ~= nil and vv.CallGamemodePaintFunc:GetBool() and vv.CallGamemodePaintFuncFirst:GetBool() == false then
                                 PaintFunc(s,w,h)
@@ -145,41 +145,41 @@ end
 function PANEL:UpdatePast()
     if self ~= nil and self:Valid() then
         table.insert(self.Past, self.ply:VoiceVolume())
-        
+
         local len = #self.Past
         if len > (vv.BarCount:GetInt()-1) then
             table.remove(self.Past, 1)
         end
     end
-end 
+end
 
 function PANEL:GetBarColor(p)
     local barcolor = Color(0,0,0)
-   
+
     for i,v in pairs(vv.BarColor) do
         if p > i then
             barcolor = v
         end
     end
-   
+
     return barcolor
 end
 
 function PANEL:VVPaint(w, h)
-	if not IsValid(self.ply) or not self:Valid() then return end
-	draw.RoundedBox(4, 0, 0, w, h, vv.BackgroundColor(self, self.ply))
-    
+  if not IsValid(self.ply) or not self:Valid() then return end
+  draw.RoundedBox(4, 0, 0, w, h, vv.BackgroundColor(self, self.ply))
+
     for i,v in pairs(self.Past) do
         local barh = v * vv.BarHeightMultiplier:GetFloat()
         local barcolor = self:GetBarColor(v * 100)
         surface.SetDrawColor(barcolor)
         surface.DrawRect(35 + i * (vv.BarDistance:GetFloat() + vv.SingleBarWidth:GetFloat()), 36 - barh, vv.SingleBarWidth:GetFloat(), barh)
     end
-    
+
     -- Draw Name
     surface.SetFont(vv.NameFont(self, self.ply))
     local w,h = surface.GetTextSize(self.ply:Nick())
-    
+
     surface.SetTextColor(vv.NameColor(self, self.ply))
     surface.SetTextPos(40, 40/2 - h/2)
     surface.DrawText(self.ply:Nick())
@@ -193,20 +193,22 @@ function PANEL:Think()
     end
 end
 
-function PANEL:FadeOut(anim, delta, data)	
-	if anim.Finished then	
-		if IsValid(PlayerVoicePanels[self.ply]) then
-			PlayerVoicePanels[self.ply]:Remove()
-			PlayerVoicePanels[self.ply] = nil
-			return
-		end		
-        
-        return 
+function PANEL:FadeOut(anim, delta, data)
+  if anim.Finished then
+
+    if IsValid(PlayerVoicePanels[self.ply]) then
+      PlayerVoicePanels[self.ply]:Remove()
+      PlayerVoicePanels[self.ply] = nil
+      return
     end
-			
-	self:SetAlpha(255 - (255 * (delta*2)))
+
+    return
+  end
+
+  self:SetAlpha(255 - (255 * (delta*2)))
 end
-derma.DefineControl("VoiceNotify", "", PANEL, "DPanel") 
+
+derma.DefineControl("VoiceNotify", "", PANEL, "DPanel")
 
 -- Support for the shitty gamemodes that like creating there own voice panel -.-
 local function HookVoiceVGUI()
@@ -217,4 +219,5 @@ local function HookVoiceVGUI()
         end
     end)
 end
+
 hook.Add("InitPostEntity", "VVHookVoiceVGUI", HookVoiceVGUI)
